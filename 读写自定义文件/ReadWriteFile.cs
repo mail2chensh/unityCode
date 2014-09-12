81a9852eee809e36a8333e526bca289aa77bf5f9
chensh <mail2chensh@gmail.com>
2014-09-12 18:01:41 +0800

    读写自定义文件

---
+++ 读写自定义文件/ReadWriteFile.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using System;
+
+public class ReadWriteFile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        writeContent(Application.dataPath, "fileName", "hello!");
+        writeContent(Application.dataPath, "fileName", "how r u");
+
+        ArrayList info = LoadContent(Application.dataPath, "fileName");
+        foreach (string str in info)
+        {
+            Debug.Log(str);
+        }
+	}
+
+    ArrayList LoadContent(string filePath, string fileName)
+    {
+        StreamReader sRead = null;
+        try
+        {
+            sRead = File.OpenText(filePath + "//" + fileName);
+        }
+        catch (Exception e)
+        {
+            e.ToString();
+            return null;
+        }
+        string line;
+        ArrayList list = new ArrayList();
+        while ((line = sRead.ReadLine()) != null)
+        {
+            list.Add(line);
+        }
+        sRead.Close();
+        sRead.Dispose();
+        return list;
+    }
+
+    void writeContent(string filePath, string fileName, string content)
+    {
+        StreamWriter swrite;
+        FileInfo file = new FileInfo(filePath + "//" + fileName);
+        if (!file.Exists)
+        {
+            swrite = file.CreateText();
+        }
+        else
+        {
+            swrite = file.AppendText();
+        }
+        swrite.WriteLine(content);
+        swrite.Close();
+        swrite.Dispose();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
